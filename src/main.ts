5c50d3c1b6fe56bc5fc15fbc3c84a7a70c7e7123
adam <adam@harpur.io>
2021-10-08 08:56:53 +0100

    feat: add stars

--- src/main.ts
+++ src/main.ts
@@ -40,6 +40,21 @@ scene.add(lightHelper, gridHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24)
+  const material = new THREE.MeshStandardMaterial({ color: lightColor })
+  const star = new THREE.Mesh(geometry, material)
+  const [x, y, z] = Array(3)
+    .fill(0)
+    .map(() => THREE.MathUtils.randFloatSpread(100))
+  star.position.set(x, y, z)
+  scene.add(star)
+}
+
+Array(200)
+  .fill(0)
+  .forEach(addStar)
+
 function rotateTorus() {
   torus.rotation.x += 0.01
   torus.rotation.y += 0.005
